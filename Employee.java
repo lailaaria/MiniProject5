fef023c20623b0f25c5c05eb6004c4b9ac5c4aba
Laila  Isaac <laila.isaac@patelhs.org>
2021-02-18 15:02:45 +0000

    added a constructor

--- Employee.java
+++ Employee.java
@@ -1,6 +1,14 @@
 public class Employee{
  //variables
- private String name
- private String jobTitle
- private int salary
+ private String name;
+ private String jobTitle;
+ private int salary;
+
+ public Employee(String employeeName, String jobPosition, int yearlyIntake){
+   name = employeeName;
+   jobTitle = jobPosition;
+   salary = yearlyIntake;
+ }
+
+ 
 }
